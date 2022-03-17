82a5e06851021c2164161263df123a8dfab2b6c8
Quang Nguyen <thanhquang0912@gmail.com>
2022-03-17 02:23:10 +0100

    a weather app for phone using WeatherAPI key

---
+++ weather-api/src/App.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom';
+const api = {
+  key:"5fb5902821de344cd33d848d18d4274a",
+  base:"http://api.openweathermap.org/data/2.5/"
+}
+function App() {
+
+  const [query, setQuery]= useState('');
+  const [weather, setWeather] = useState({});
+  const search = evt =>{
+    if(evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(weather)
+    });
+    }
+  }
+  return (
+    <div className={(typeof weather.main !='undefined') ? ((weather.main.temp>15)?'app warm':'app'):'app'}>
+      <main>
+        <div className='search-box'>
+          <input
+          type="text"
+          className='search-bar'
+          placeholder='Search ... ' 
+          onChange={e=>setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}/>
+        </div>
+    {(typeof weather.main !="undefined") ? (
+      <div>
+      <div className='location-box'>
+          <div className='location'>{weather.name}, {weather.sys.country }</div>
+          <div className='date'>{new Date().toDateString()}</div>
+      </div>
+
+      <div className='weather-box'>
+        <div className='temp'>{Math.round(weather.main.temp)}°C</div>
+        <div className='weather'>{weather.weather[0].main}</div>
+      </div>
+
+  </div>
+    ):('')}
+    
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
